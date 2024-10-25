72d9593cf5da7c3c94b9a1bcdff87d0db8f58f63
Shivani Singh <singh.shivani9811@gmail.com>
2024-10-25 20:21:58 +0530

    Add files via upload

---
+++ Data_Visualization/logscale.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(1, 100, 100)
+y = np.log(x)
+
+plt.plot(x, y)
+plt.title('Logarithmic Scale Example')
+plt.xlabel('X-axis')
+plt.show()
