d2824525cc0b9ee29616377c08899fcd7d2a90aa
Shivani Singh <singh.shivani9811@gmail.com>
2024-10-25 20:40:49 +0530

    Add files via upload

---
+++ DeepLearning_VIDPR/vid_stack.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+cap1 = cv2.VideoCapture('vd1.mp4')
+cap2 = cv2.VideoCapture('vd2.mp4')
+
+if not cap1.isOpened() or not cap2.isOpened():
+    print("Error: Could not open video.")
+    exit()
+
+while True:
+    ret1, frame1 = cap1.read()
+    ret2, frame2 = cap2.read()
+
+    if not ret1 or not ret2:
+        break
+
+    frame1 = cv2.resize(frame1, (640, 360))
+    frame2 = cv2.resize(frame2, (640, 360))
+
+    # for horizontal video concat
+    #h_concat = np.hstack((frame1, frame2))
+
+    #for vertical video concat
+    v_concat = np.vstack((frame1, frame2))
+
+    cv2.imshow('Concatenated Video', v_concat) # write h_concat for horizontal and v_concat for vertical
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+cap1.release()
+cap2.release()
+cv2.destroyAllWindows()
