bd7d2aa6b5e5fb215d9fd6adbfcbfd80aeda2762
Shivani Singh <singh.shivani9811@gmail.com>
2024-10-25 20:26:52 +0530

    Delete threshhold12.py

--- threshhold12.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread('1.jpg', 0)
-_, threshold = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-
-cv2.imshow('Threshold Image', threshold)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
